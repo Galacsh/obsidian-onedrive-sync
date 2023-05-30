f303fcb24f184c4075259254197b2d5339435984
Galacsh <galacsh.dev@gmail.com>
2023-05-31 00:38:13 +0900

    fix: wrong path

--- src/settings/default.ts
+++ src/settings/default.ts
@@ -3,7 +3,7 @@ import { TSettings } from "src/types";
 const DEFAULT_SETTINGS: TSettings = {
 	deltaLink: null,
 	oneDriveIndex: {},
-	ignore: ["\\.obsidian\\/plugins\\/obsidian-onedrive-sync\\/data\\.json", "\\.git"],
+	ignore: ["\\.obsidian\\/plugins\\/onedrive-sync\\/data\\.json", "\\.git"],
 };
 
 export default DEFAULT_SETTINGS;
